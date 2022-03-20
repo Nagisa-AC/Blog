c05df0ceb25f12b411a7aff54ed681828ddecd0d
Ibrahim Kebbeh <ikebbeh444@gmail.com>
2022-03-20 00:43:23 -0400

    Added ejs templates and updated home page title

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("going to sleep now");
+const express = require('express');
+const app = express();
+const port = 2000;
+
+app.set('view engine', 'ejs');
+app.set('views', 'views')
+
+
+// Handling requests
+app.get('/', (req, res, next) => {
+    res.render('index');
+})
+
+app.get('/about', (req, res, next) => {
+    res.render('about');
+});
+
+app.get('/blogs/create', (req, res, next) => {
+    res.render('create');
+});
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
+
+// Listener
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
